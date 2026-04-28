18d63e7a971f10bd192a9dbd24e78d6b0c0ca53d
Luka Ladisic <l.ladisic@codenovasolutions.net>
2026-04-28 08:18:33 +0200

    add basic TCP HTTP server

--- Http/HttpServer.cs
+++ Http/HttpServer.cs
@@ -16,6 +16,40 @@ namespace MiniHttpServer.Http
             _listener = new TcpListener(System.Net.IPAddress.Any, port);
         }
 
-        public 
+        public void Start()
+        {
+            _listener.Start();
+
+            Console.WriteLine("HTTP Server started on port " + ((System.Net.IPEndPoint)_listener.LocalEndpoint).Port);
+
+            while (true)
+            {
+                TcpClient client = _listener.AcceptTcpClient();
+
+                using NetworkStream stream = client.GetStream();
+
+                byte[] buffer = new byte[1024];
+                int bytesRead = stream.Read(buffer, 0, buffer.Length);
+
+                string rawRequest = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+
+                Console.WriteLine("----- RAW HTTP REQUEST -----");
+                Console.WriteLine(rawRequest);
+
+                string responseBody = "Hello from MiniHttpServer";
+
+                string response = "HTTP/1.1 200 OK\r\n" +
+                                  "Content-Type: text/plain\r\n" +
+                                  "Content-Length: " + responseBody.Length + "\r\n" +
+                                  "\r\n" +
+                                  responseBody;
+
+                byte[] responseBytes = Encoding.UTF8.GetBytes(response);
+
+                stream.Write(responseBytes, 0, responseBytes.Length);
+
+                client.Close();
+            }
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,10 +1,12 @@
-﻿namespace MiniHttpServer
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
+﻿using MiniHttpServer.Http;
 
-        }
+namespace MiniHttpServer;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        HttpServer server = new HttpServer(5000);
+        server.Start();
     }
 }
